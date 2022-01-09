db981ac4597cd3e1ab94a8b5e306be3537e7cf48
HarkaranSinghBal <97388224+HarkaranSinghBal@users.noreply.github.com>
2022-01-09 17:32:18 +0530

    Add files via upload

---
+++ asg1_CSE_21103058_output.py
@@ -0,0 +1,32 @@
+Q.1.- Give any three integers: 
+a = 3
+b = 4
+c = 5
+The average of these integers is  4.0
+
+Q.2.- Please give details for evaluating the amount of tax that you will receive: 
+Gross income (in dollars): 20000
+Number of dependants : 3
+The amount of tax is:  200.0
+
+Q.3.- Please enter your details below: 
+Enter your SID: 21103058
+Enter your name: Harkaran Singh Bal
+Enter your gender, Use Gender values ‘F’, ‘M’, ‘U’ (For Unknown): M
+Enter your course name: CSE
+Enter your CGPA: 8
+The list of the given details are:  [21103058, 'Harkaran Singh Bal', 'M', 'CSE', 8.0]
+
+Q.4.- Enter the marks of 5 students below: 
+Student1 = 64
+Student2 = 73
+Student3 = 44
+Student4 = 68
+Student5 = 89
+The marks of the students in sorted manner is:  [44, 64, 68, 73, 89]
+
+Q.5.(a)- The list of given colors is:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+The new list of colors after removing black is:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+Q.5.(b)-The list of given colors is:  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+The new list of colors after replacing black and pink with purple is:  ['Red', 'Green', 'White', 'Purple', 'Yellow']

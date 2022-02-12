1a1a4433c0f2755298211c736c6431662aecfe30
HarkaranSinghBal <97388224+HarkaranSinghBal@users.noreply.github.com>
2022-02-12 18:23:28 +0530

    Update asg3_21103058_output.py

--- asg3_21103058_output.py
+++ asg3_21103058_output.py
@@ -101,6 +101,7 @@ The name of the student is pqr
 
 
 Question.7.-
+Enter the number of terms upto which you wish to find the fibbonaci sequence: 10
 0 , 1 , 1 , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55 , 89 , 144 , 
 The average upto 10 terms is:  14.4
 
